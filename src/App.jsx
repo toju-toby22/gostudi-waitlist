8efab8bac5d6b86020f3774c1018cb49e46c79b5
Tobi Otoju <ttotoju@gmail.com>
2025-05-27 00:29:46 +0100

    new push

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WaitlistPage from './Waitlistpage'
+import WaitlistPage from './waitlistpage'
 
 function App() {
   const [count, setCount] = useState(0)
